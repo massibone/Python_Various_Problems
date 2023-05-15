5b696a61f97f58c5562f420e633fe7cb6d2ffd10
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-05-15 13:08:54 +0200

    Create compare_similar_files.py

---
+++ SEARCH/compare_similar_files.py
@@ -0,0 +1,21 @@
+import os
+
+def compare_similar_files(directory):
+    """Confronta file con nomi simili all'interno di una directory"""
+    similar_files = []
+    filenames = os.listdir(directory)
+    for filename in filenames:
+        base_name, ext = os.path.splitext(filename)
+        pair_name = base_name + '_pair' + ext
+        pair_path = os.path.join(directory, pair_name)
+        if pair_name in filenames:
+            similar_files.append((filename, pair_name))
+    return similar_files
+
+# Esempio di utilizzo
+directory_path = '/percorso/alla/cartella'
+similar_files = compare_similar_files(directory_path)
+
+# Stampa dei file con nomi simili
+for file1, file2 in similar_files:
+    print(f"I file {file1} e {file2} hanno nomi simili.")
