915d95af4dd8d3462e4cf9554b6603939c8c74ed
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-12-23 08:03:01 +0100

    25**2 or 65**2 or 75**2 etc etc

--- Calcola quadrato.py
+++ Calcola quadrato.py
@@ -1,1 +1,13 @@
 Calcola quadrato. Es 25**2= 5**2+(2+1)**2=925
+
+def calcola_quadrato(numero):
+    cifre = [int(digito) for digito in str(numero)]
+    primo_digito = cifre[0]
+    secondo_digito = cifre[1] if len(cifre) > 1 else 0
+
+    risultato = primo_digito**2 + (secondo_digito + 1)**2
+    return f"{numero}**2 = {primo_digito}**2 + ({secondo_digito} + 1)**2 = {risultato}"
+
+for num in range(10, 100):
+    print(calcola_quadrato(num))
+
