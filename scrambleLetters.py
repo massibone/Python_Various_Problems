f4386323a458afee9c2ea80db3e9c40f8ec4b680
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-03-22 08:00:41 +0100

    Update scrambleLetters.py

--- scrambleLetters.py
+++ scrambleLetters.py
@@ -1,1 +1,61 @@
 """Scramble the letters of words"""
+
+import argparse
+import os
+import re
+import random
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Scramble the letters of words',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('text', metavar='text', help='Input text or file')
+
+    parser.add_argument('-s',
+                        '--seed',
+                        help='Random seed',
+                        metavar='seed',
+                        type=int,
+                        default=None)
+
+    args = parser.parse_args()
+
+    if os.path.isfile(args.text):
+        args.text = open(args.text).read().rstrip()
+
+    return args
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    random.seed(args.seed)
+    splitter = re.compile("([a-zA-Z](?:[a-zA-Z']*[a-zA-Z])?)")
+
+    for line in args.text.splitlines():
+        print(''.join(map(scramble, splitter.split(line))))
+
+
+# --------------------------------------------------
+def scramble(word):
+    """For words over 3 characters, shuffle the letters in the middle"""
+
+    if len(word) > 3 and re.match(r'\w+', word):
+        middle = list(word[1:-1])
+        random.shuffle(middle)
+        word = word[0] + ''.join(middle) + word[-1]
+
+    return word
+
+
+word="massiMOOOooo"
+print(scramble(word))
+
+
