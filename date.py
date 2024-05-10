066dc6fc0f19988f7298a65eade40131612ce4c2
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-05-10 08:11:17 +0200

    Update date.py

--- date.py
+++ date.py
@@ -1,1 +1,25 @@
+date = input("Enter your date of birth in YYYYMMDD format: > ")
+sum_list = []
 
+def digitOfLife(date):
+    sum = 0
+    if(len(date) > 8):
+        print("Input data too long")
+        return
+    else:
+        date_list = []
+        for char in date:
+            date_list.append(int(char))
+            
+        for num in date_list:
+            sum+=num
+            if sum > 9:
+                sum = sum%10 +sum//10
+    return sum
+    
+print(digitOfLife(date))
+------------------------------------
+Enter your date of birth in YYYYMMDD format: > 19991229
+6
+Enter your date of birth in YYYYMMDD format: > 20000101
+4
