8c06030f2e45704571c033e704428285408c34c6
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-11-29 08:13:51 +0100

    Create somma_liste.py

---
+++ somma_liste.py
@@ -0,0 +1,7 @@
+'''
+Scrivere un programma che, 
+date due liste di interi di uguale lunghezza ne crei una che contenga la somma di ogni elemento, 
+indice per indice (quindi l'i-esimo elemento della lista somma è 
+l'addizione fra l'i-esimo elemento della prima lista e della seconda)
+'''
+
