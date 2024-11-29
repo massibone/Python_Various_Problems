4574539bc8ecbfaea0a7909eba4fd2b39c8ec6dc
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-11-29 08:14:13 +0100

    Update somma_liste.py

--- somma_liste.py
+++ somma_liste.py
@@ -5,3 +5,12 @@ indice per indice (quindi l'i-esimo elemento della lista somma è
 l'addizione fra l'i-esimo elemento della prima lista e della seconda)
 '''
 
+l1 = [1,2,3,4,5]
+l2 = [6,7,8,9,10]
+
+l_sum = []
+
+for i in range(len(l1)):
+    l_sum.append(l1[i] + l2[i])
+
+print(l_sum)
