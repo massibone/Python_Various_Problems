6ca09f0fc630bb26c521ca0edca71df3936aaf0f
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-02-08 09:13:37 +0100

    Update palindroma_txt.py

--- pcep_preparation/palindroma_txt.py
+++ pcep_preparation/palindroma_txt.py
@@ -1,1 +1,9 @@
+def is_palindrome(word):
+  """Verifica se una parola è un palindromo.
 
+  Args:
+    word: La parola da controllare.
+
+  Returns:
+    True se la parola è un palindromo, False altrimenti.
+  """
