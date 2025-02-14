7476b8f7e9f4eaed9b81a33bc284da8291412b59
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-02-14 08:03:08 +0100

    Create ricerca_binaria.py

---
+++ pcep_preparation/ricerca_binaria.py
@@ -0,0 +1,13 @@
+'''nell'uso avanzato di costrutti  else'''
+def ricerca_binaria(lista, target):
+    sinistra, destra = 0, len(lista) - 1
+    while sinistra <= destra:
+        mezzo = (sinistra + destra) // 2
+        if lista[mezzo] == target:
+            return mezzo
+        elif lista[mezzo] < target:
+            sinistra = mezzo + 1
+        else:
+            destra = mezzo - 1
+    else:
+        return -1  # Elemento non trovato
