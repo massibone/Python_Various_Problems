026c9a0ea38aab71688d1a08eeb22921106ae846
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-02-19 08:07:18 +0100

    Update palindromaNLP.py

--- pcep_preparation/palindromaNLP.py
+++ pcep_preparation/palindromaNLP.py
@@ -1,1 +1,8 @@
+import string
+
+def is_palindrome(word):
+  """Verifica se una parola è un palindromo, ignorando punteggiatura e maiuscole/minuscole."""
+  word = word.lower()
+  word = word.translate(str.maketrans('', '', string.punctuation))
+  return word == word[::-1]
 
