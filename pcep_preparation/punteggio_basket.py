a465c01b402bea16a5509cc184d6f7cb77ecbda5
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-02-24 08:28:41 +0100

    Update punteggio_basket.py

--- pcep_preparation/punteggio_basket.py
+++ pcep_preparation/punteggio_basket.py
@@ -1,1 +1,10 @@
+'''
+Esempio:
 
+Supponiamo di voler calcolare il punteggio in una partita di basket, considerando punti segnati, assist e palle perse. Potremmo definire le seguenti regole:
+
+1 punto per ogni canestro segnato.
+0.5 punti per ogni assist.
+-0.25 punti per ogni palla persa.
+Bonus di 2 punti se il giocatore ha segnato più di 20 punti.
+'''
