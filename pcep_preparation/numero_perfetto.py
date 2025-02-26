3c0c562a9b145b33098132ef66e57068e02a3cc1
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-02-26 08:11:34 +0100

    Update numero_perfetto.py

--- pcep_preparation/numero_perfetto.py
+++ pcep_preparation/numero_perfetto.py
@@ -1,9 +1,9 @@
 def trova_numero_perfetto(limite):
-    """Trova il primo numero perfetto minore o uguale a limite.
-
+    '''
+    Trova il primo numero perfetto minore o uguale a limite.
     Args:
         limite: Il limite superiore della ricerca.
-
     Returns:
         Il numero perfetto trovato, altrimenti None.
-    """
+    '''
+    
