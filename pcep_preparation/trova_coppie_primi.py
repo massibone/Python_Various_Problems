94cff835f50dbf5dcaef8b8ab02cb28c4d3b75b5
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-03-10 08:19:48 +0100

    Update trova_coppie_primi.py

--- pcep_preparation/trova_coppie_primi.py
+++ pcep_preparation/trova_coppie_primi.py
@@ -1,1 +1,12 @@
-
+'''
+Trova tutte le coppie di numeri primi minori di 100
+'''
+def primi_coppie(n):
+    primi = []
+    for i in range(2, n):
+        for j in range(2, int(i**0.5) + 1):
+            if i % j == 0:
+                break
+        else:
+            primi.append(i)
+    return [(x, y) for x in primi for y in primi if x < y]
