da77a07850d0586deeba9a934895b3210c0e99e9
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-03-17 08:36:27 +0100

    Update oc_file.py

--- pcep_preparation/oc_file.py
+++ pcep_preparation/oc_file.py
@@ -1,1 +1,14 @@
+'''
+Apri un file, leggi le righe e chiudilo correttamente.
+'''
 
+with open('file.txt', 'r') as f:
+    for line in f:
+        try:
+            # Processa la linea
+        except ValueError:
+            print("Errore nella linea:", line)
+    else:
+        print("File elaborato correttamente.")
+finally:
+    f.close()
