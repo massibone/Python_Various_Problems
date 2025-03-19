b2764a60f2f67ec00c286347c9cc3d720c5e5d8d
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-03-19 08:04:47 +0100

    Update tabella_moltiplicazione_grande.py

--- pcep_preparation/tabella_moltiplicazione_grande.py
+++ pcep_preparation/tabella_moltiplicazione_grande.py
@@ -15,3 +15,10 @@ def tabella_moltiplicazione_numpy_grande(righe, colonne):
     colonne_array = np.arange(1, colonne + 1)
     tabella = righe_array * colonne_array
     return tabella
+
+# Esempio: tabella 1000x1000
+tabella_grande = tabella_moltiplicazione_numpy_grande(1000, 1000)
+print(tabella_grande)
+
+# Per tabelle ancora più grandi (es. 10000x10000), considera di salvare l'array in un file
+# np.save('tabella_grande.npy', tabella_grande)
