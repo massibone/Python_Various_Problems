12e01a59c2bdd1c92f15a182b37bee3a14739adc
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-03-19 08:04:30 +0100

    Update tabella_moltiplicazione_grande.py

--- pcep_preparation/tabella_moltiplicazione_grande.py
+++ pcep_preparation/tabella_moltiplicazione_grande.py
@@ -1,1 +1,17 @@
+import numpy as np
 
+def tabella_moltiplicazione_numpy_grande(righe, colonne):
+    """
+    Crea una tabella di moltiplicazione di grandi dimensioni utilizzando NumPy.
+
+    Args:
+        righe: Numero di righe.
+        colonne: Numero di colonne.
+
+    Returns:
+        Array NumPy contenente la tabella.
+    """
+    righe_array = np.arange(1, righe + 1).reshape(-1, 1)
+    colonne_array = np.arange(1, colonne + 1)
+    tabella = righe_array * colonne_array
+    return tabella
