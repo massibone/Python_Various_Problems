4b88c218eb6dfadcc6d3e90dbbba683c0a2a1e8c
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-03-20 08:11:46 +0100

    Update tabelle_grandi_generatori.py

--- pcep_preparation/tabelle_grandi_generatori.py
+++ pcep_preparation/tabelle_grandi_generatori.py
@@ -1,3 +1,8 @@
 '''
 Per tabelle così grandi da non poter essere memorizzate interamente in RAM, i generatori sono la soluzione.
 '''
+
+def genera_riga_moltiplicazione(riga, colonne):
+    """Genera una riga della tabella di moltiplicazione."""
+    for colonna in range(1, colonne + 1):
+        yield riga * colonna
