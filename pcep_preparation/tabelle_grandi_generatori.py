645d52bee5cab6bb8b8c29e2761081f2053fee83
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-03-21 08:23:39 +0100

    Update tabelle_grandi_generatori.py

--- pcep_preparation/tabelle_grandi_generatori.py
+++ pcep_preparation/tabelle_grandi_generatori.py
@@ -6,3 +6,8 @@ def genera_riga_moltiplicazione(riga, colonne):
     """Genera una riga della tabella di moltiplicazione."""
     for colonna in range(1, colonne + 1):
         yield riga * colonna
+
+def tabella_moltiplicazione_generatore_grande(righe, colonne):
+    """Genera una tabella di moltiplicazione di grandi dimensioni utilizzando generatori."""
+    for riga in range(1, righe + 1):
+        yield genera_riga_moltiplicazione(riga, colonne)
