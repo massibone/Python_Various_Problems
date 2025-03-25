1dd184ab40bf64be7ca3f76e83ca4036a1c5d493
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-03-25 08:40:37 +0100

    Update applica_filtro.py

--- pcep_preparation/applica_filtro.py
+++ pcep_preparation/applica_filtro.py
@@ -1,6 +1,10 @@
 '''
-Applica filtri a immagini di grandi dimensioni. Un filtro comune è un filtro di convoluzione, che coinvolge la moltiplicazione di una matrice di filtri con una matrice di pixel.
+Applica filtri a immagini di grandi dimensioni. Un filtro comune è un filtro di convoluzione, 
+che coinvolge la moltiplicazione di una matrice di filtri con una matrice di pixel.
 
-Matrici di grandi dimensioni: Le immagini ad alta risoluzione possono avere milioni di pixel, e le matrici di filtri possono essere di dimensioni considerevoli (ad esempio, 100x100).
-Moltiplicazioni ripetute: Il filtro viene applicato a ogni pixel dell'immagine, quindi le moltiplicazioni vengono eseguite milioni di volte.
+Matrici di grandi dimensioni: 
+Le immagini ad alta risoluzione possono avere milioni di pixel, 
+e le matrici di filtri possono essere di dimensioni considerevoli (ad esempio, 100x100).
+Moltiplicazioni ripetute: Il filtro viene applicato a ogni pixel dell'immagine, 
+quindi le moltiplicazioni vengono eseguite milioni di volte.
 '''
