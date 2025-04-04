c345b17ebf466a4ee2e4a5ddc833e533e17de72f
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-04-04 08:04:35 +0200

    Update comprehension.py

--- pcep_preparation/comprehension.py
+++ pcep_preparation/comprehension.py
@@ -1,1 +1,8 @@
-
+'''
+Le comprehension sono un modo conciso per creare nuove liste, dizionari o altri tipi di dati in Python, utilizzando una sintassi specifica. Sono utilizzate per semplificare il codice e renderlo più leggibile.
+Comprehension per liste: [x**2 for x in range(10) if x % 2 == 0].
+per dizionari:
+dizionario = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+dizionario_pari = {chiave: valore for chiave, valore in dizionario.items() if valore % 2 == 0}
+print(dizionario_pari)  # {'b': 2, 'd': 4}
+'''
