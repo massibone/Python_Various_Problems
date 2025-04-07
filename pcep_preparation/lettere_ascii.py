77928e3eef175e38429890f7d08782cb9f27fb33
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-04-07 08:16:37 +0200

    Update lettere_ascii.py

--- pcep_preparation/lettere_ascii.py
+++ pcep_preparation/lettere_ascii.py
@@ -1,3 +1,5 @@
 '''
 Creare un dizionario con le lettere dell'alfabeto come chiavi e i loro codici ASCII come valori
 '''
+dizionario = {lettera: ord(lettera) for lettera in 'abcdefghijklmnopqrstuvwxyz'}
+print(dizionario)  # {'a': 97, 'b': 98, 'c': 99, ...}
