1a0c562c752f886c43d46170fb0cecd3461fe095
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-04-14 08:17:23 +0200

    Update es2.py

--- pcep_preparation/es2.py
+++ pcep_preparation/es2.py
@@ -10,3 +10,11 @@ testo = ''.join(e for e in testo if e.isalnum() or e.isspace()).lower()
 
 # Divide il testo in parole
 parole = testo.split()
+
+
+# Crea una lista di parole con 4 lettere che iniziano con "a"
+parole_4_lettere_a = [parola for parola in parole if len(parola) == 4 and parola.startswith('a')]
+
+# Stampa le parole con 4 lettere che iniziano con "a"
+for parola in parole_4_lettere_a:
+    print(parola)
