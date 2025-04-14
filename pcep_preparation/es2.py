a018262280ce2892cd325e9fd3f2d8280b8aff45
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-04-14 08:17:05 +0200

    Update es2.py

--- pcep_preparation/es2.py
+++ pcep_preparation/es2.py
@@ -4,3 +4,9 @@ Trovare parole con 4 lettere che iniziano con una lettera specifica
 # Legge il testo da un file
 with open('testo.txt', 'r') as file:
     testo = file.read()
+
+# Sostituisce i caratteri non alfabetici con spazi
+testo = ''.join(e for e in testo if e.isalnum() or e.isspace()).lower()
+
+# Divide il testo in parole
+parole = testo.split()
