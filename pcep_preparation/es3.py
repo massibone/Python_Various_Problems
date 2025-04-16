328d229f2579a3c307c59f787610072602520330
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-04-16 08:08:08 +0200

    Update es3.py

--- pcep_preparation/es3.py
+++ pcep_preparation/es3.py
@@ -5,3 +5,8 @@ Trovare parole con 4 lettere che contengono una lettera specifica
 with open('testo.txt', 'r') as file:
     testo = file.read()
 
+# Sostituisce i caratteri non alfabetici con spazi
+testo = ''.join(e for e in testo if e.isalnum() or e.isspace()).lower()
+
+# Divide il testo in parole
+parole = testo.split()
