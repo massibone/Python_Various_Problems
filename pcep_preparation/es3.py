8a89273f8ccb900117d0df869069a20bb3f4986b
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-04-16 08:08:20 +0200

    Update es3.py

--- pcep_preparation/es3.py
+++ pcep_preparation/es3.py
@@ -10,3 +10,10 @@ testo = ''.join(e for e in testo if e.isalnum() or e.isspace()).lower()
 
 # Divide il testo in parole
 parole = testo.split()
+
+# Crea una lista di parole con 4 lettere che contengono "e"
+parole_4_lettere_e = [parola for parola in parole if len(parola) == 4 and 'e' in parola]
+
+# Stampa le parole con 4 lettere che contengono "e"
+for parola in parole_4_lettere_e:
+    print(parola)
