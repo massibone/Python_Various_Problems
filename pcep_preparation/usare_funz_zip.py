76e2cb46a22ef752a0f0a971389a09bb4494f243
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-04-18 08:08:27 +0200

    Update usare_funz_zip.py

--- pcep_preparation/usare_funz_zip.py
+++ pcep_preparation/usare_funz_zip.py
@@ -1,1 +1,10 @@
+'''
+Le liste devono avere la stessa lunghezza. Se una lista è più corta, zip() si fermerà alla lunghezza della lista più breve.'''
+# Esempio di liste
+chiavi = ["nome", "età", "città"]
+valori = ["Alice", 30, "Roma"]
 
+# Creazione del dizionario usando zip()
+dizionario = dict(zip(chiavi, valori))
+
+print(dizionario)
