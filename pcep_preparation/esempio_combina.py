8bac48fe1b4b050618bea3c984bb9b9ce9d1a372
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-04-25 10:08:14 +0200

    Update esempio_combina.py

--- pcep_preparation/esempio_combina.py
+++ pcep_preparation/esempio_combina.py
@@ -1,6 +1,7 @@
 
 '''
-Creare un elenco di studenti con nome, materia, e voto, ordinare gli studenti per voto decrescente, e stampare una tabella con posizione, nome, materia, e voto.
+Creare un elenco di studenti con nome, 
+materia, e voto, ordinare gli studenti per voto decrescente, e stampare una tabella con posizione, nome, materia, e voto.
 Esempio che combinerà zip(), enumerate(), e sorted() con una funzione personalizzata per il key, in un unico programma
 '''
 
