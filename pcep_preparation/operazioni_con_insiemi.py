1777aae8797a6c87ab1b2e955afb750b2e68ecd6
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-05-05 08:00:10 +0200

    Update operazioni_con_insiemi.py

--- pcep_preparation/operazioni_con_insiemi.py
+++ pcep_preparation/operazioni_con_insiemi.py
@@ -2,3 +2,9 @@
 Operazioni con insiemi: 
 unione, intersezione e differenza.
 '''
+# Definizione di due insiemi di esempio
+insieme_A = {1, 2, 3, 4, 5}
+insieme_B = {4, 5, 6, 7, 8}
+
+print("Insieme A:", insieme_A)
+print("Insieme B:", insieme_B)
