78136f7163aa6180dc859d20543fcac7cd2c2f10
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-05-05 08:00:41 +0200

    Update operazioni_con_insiemi.py

--- pcep_preparation/operazioni_con_insiemi.py
+++ pcep_preparation/operazioni_con_insiemi.py
@@ -9,3 +9,7 @@ insieme_B = {4, 5, 6, 7, 8}
 
 print("Insieme A:", insieme_A)
 print("Insieme B:", insieme_B)
+
+# 1. Unione (elementi presenti in A o B)
+unione = insieme_A.union(insieme_B)          # Oppure: insieme_A | insieme_B
+print("\nUnione (A ∪ B):", unione)
