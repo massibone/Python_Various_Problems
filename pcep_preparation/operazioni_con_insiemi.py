51aa3b4472a1d10e4f19aab7f2c466916a1d70b0
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-05-06 09:35:24 +0200

    Update operazioni_con_insiemi.py

--- pcep_preparation/operazioni_con_insiemi.py
+++ pcep_preparation/operazioni_con_insiemi.py
@@ -17,3 +17,12 @@ print("\nUnione (A ∪ B):", unione)
 # 2. Intersezione (elementi comuni a A e B)
 intersezione = insieme_A.intersection(insieme_B)  # Oppure: insieme_A & insieme_B
 print("Intersezione (A ∩ B):", intersezione)
+
+
+# 3. Differenza (elementi in A ma non in B)
+differenza_A_B = insieme_A.difference(insieme_B)  # Oppure: insieme_A - insieme_B
+print("Differenza (A - B):", differenza_A_B)
+
+# Differenza inversa (elementi in B ma non in A)
+differenza_B_A = insieme_B.difference(insieme_A)  # Oppure: insieme_B - insieme_A
+print("Differenza (B - A):", differenza_B_A)
