b3b56dd3f5955c741145c002034251a249905326
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-05-08 08:08:56 +0200

    Update differenze.py

--- pcep_preparation/differenze.py
+++ pcep_preparation/differenze.py
@@ -1,3 +1,7 @@
 '''
 Differenze tra list, tuple, e set
 '''
+# --- ESEMPIO DI LIST (elenco ordinato, mutabile, consente duplicati) ---
+print("\n--- LIST ---")
+lista = [1, 2, 3, 3, 4]
+print("Lista iniziale:", lista)
