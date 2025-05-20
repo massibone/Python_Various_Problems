32fb076413553be0e5e1b254a8c32a15c14cc3c6
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-05-20 08:08:42 +0200

    Update esempio_namespaces.py

--- pcep_preparation/esempio_namespaces.py
+++ pcep_preparation/esempio_namespaces.py
@@ -2,3 +2,5 @@
 I namespaces in Python sono effettivamente simili a dizionari, 
 ma con alcune differenze importanti. Un namespace è un insieme di nomi che sono associati a oggetti, come variabili, funzioni, classi, ecc. Si possono utilizzare i namespaces per esporre solo le funzioni e le variabili che si desidera rendere pubbliche, mentre si tengono private le altre.
 '''
+#Supponiamo di avere un modulo chiamato math_utils.py che contiene diverse funzioni matematiche:
+# math_utils.py
