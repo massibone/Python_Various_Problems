dbf89b36312a365dd62062ac890a80cc4cd58581
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-05-29 08:06:17 +0200

    Update esempio_args_kwargs_calc_prezzo_finale.py

--- pcep_preparation/esempio_args_kwargs_calc_prezzo_finale.py
+++ pcep_preparation/esempio_args_kwargs_calc_prezzo_finale.py
@@ -1,3 +1,4 @@
 '''
-Calcolo prezzo finale: dimostra come combinare parametri con valori predefiniti (sconto=0, tassa=0.22), *args per costi variabili e **kwargs per opzioni aggiuntive. 
+Calcolo prezzo finale: dimostra come combinare parametri con valori predefiniti 
+(sconto=0, tassa=0.22), *args per costi variabili e **kwargs per opzioni aggiuntive. 
 '''
