8bdb5928c982cbfb4389208ba0c55e2fb6e2d462
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-06-09 08:22:39 +0200

    Update esempio_args_kwargs_builder_sql.py

--- pcep_preparation/esempio_args_kwargs_builder_sql.py
+++ pcep_preparation/esempio_args_kwargs_builder_sql.py
@@ -1,1 +1,14 @@
-
+# Builder per query SQL dinamiche
+def costruisci_query(tabella, operazione="SELECT", colonne=None, *condizioni, **parametri):
+    """
+    Costruisce query SQL dinamiche
+    
+    Args:
+        tabella: nome della tabella
+        operazione: tipo di operazione (default "SELECT")
+        colonne: liste delle colonne (default tutte con *)
+        *condizioni: condizioni WHERE multiple
+        **parametri: parametri aggiuntivi (LIMIT, ORDER BY, etc.)
+    """
+    query_parts = []
+    
