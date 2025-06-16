6ae2cab1be7c499454522844465e42bbf4430bb4
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-06-16 09:03:12 +0200

    Update scope_locale.py

--- pcep_preparation/scope_locale.py
+++ pcep_preparation/scope_locale.py
@@ -1,3 +1,8 @@
 # Gestione di un contatore globale con scope locale e globale
 contatore_globale = 0  # Variabile globale
 nome_app = "Sistema Contatori"  # Variabile globale
+
+
+def incrementa_contatore():
+    """Dimostra l'uso di variabili globali e locali"""
+    global contatore_globale  # Dichiara che vogliamo modificare la variabile globale
