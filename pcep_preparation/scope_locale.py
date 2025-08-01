a33a19918ec2509e5284e54e0271bbe11c3bf17d
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-08-01 08:28:33 +0200

    Update scope_locale.py

--- pcep_preparation/scope_locale.py
+++ pcep_preparation/scope_locale.py
@@ -1,4 +1,4 @@
-e# Gestione di un contatore globale con scope locale e globale
+# Gestione di un contatore globale con scope locale e globale
 contatore_globale = 0  # Variabile globale
 nome_app = "Sistema Contatori"  # Variabile globale
 
