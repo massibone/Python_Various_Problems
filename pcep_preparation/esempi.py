5be0d54cc4f4d49d0739a6d298b1e6696f2ac7d8
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-08-12 08:20:51 +0200

    Update esempi.py

--- pcep_preparation/esempi.py
+++ pcep_preparation/esempi.py
@@ -1,3 +1,4 @@
 '''
-Esempio di script Python che utilizza vari moduli standard come os, sys, math, e datetime per eseguire diverse operazioni. 
+Esempio di script Python che utilizza vari moduli standard come 
+os, sys, math, e datetime per eseguire diverse operazioni. 
 '''
