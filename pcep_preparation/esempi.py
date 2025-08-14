306bc985b70d5cf3fc48c5a56e7612992605578c
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-08-14 08:09:02 +0200

    Update esempi.py

--- pcep_preparation/esempi.py
+++ pcep_preparation/esempi.py
@@ -7,3 +7,21 @@ import os
 import sys
 import math
 import datetime
+def esegui_operazioni_os():
+    print("Esempio os:")
+    print("Directory corrente:", os.getcwd())
+
+    # Crea una nuova directory
+    nuova_dir = "nuova_directory"
+    if not os.path.exists(nuova_dir):
+        os.mkdir(nuova_dir)
+        print(f"Directory '{nuova_dir}' creata con successo.")
+    else:
+        print(f"La directory '{nuova_dir}' esiste già.")
+
+    # Cambia directory
+    os.chdir(nuova_dir)
+    print("Lista dei file e delle directory nella directory corrente:", os.listdir())
+
+    # Operazione di ritorno alla directory precedente
+    os.chdir("..")
