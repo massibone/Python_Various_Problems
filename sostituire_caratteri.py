0c8d73d3dce0dd6ef7e20bac559600b7f7c776ad
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-09-10 08:14:07 +0200

    Update sostituire_caratteri.py

--- sostituire_caratteri.py
+++ sostituire_caratteri.py
@@ -1,1 +1,14 @@
+'''
+sostituisce - e spazio con spazio vuoto e infatti da 4111-1111-4555-1142 
+restituisce
+4111-1111-4555-1142 
+'''
+def main():
+    card_number = '4111-1111-4555-1142'
+    card_translation = str.maketrans({'-': '', ' ': ''})
+    translated_card_number = card_number.translate(card_translation)
+
+    print(translated_card_number)
+
+main()
 
