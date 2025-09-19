555c47dcae848995ccabfc3ea38b59f0bf31f1f1
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-09-19 08:13:01 +0200

    Update raise_file_non_valido.py

--- pcep_preparation/raise_file_non_valido.py
+++ pcep_preparation/raise_file_non_valido.py
@@ -1,1 +1,16 @@
+class FormatoFileNonSupportatoError(Exception):
+  pass
 
+def leggi_file(nome_file):
+  if not nome_file.endswith(".txt"):
+    raise FormatoFileNonSupportatoError("Solo file di testo sono supportati")
+with open(nome_file, "r") as file:
+contenuto = file.read()
+return contenuto
+
+try:
+nome_file = "esempio.pdf"
+contenuto = leggi_file(nome_file)
+print("Contenuto del file:", contenuto)
+except FormatoFileNonSupportatoError as e:
+print("Errore:", e)
