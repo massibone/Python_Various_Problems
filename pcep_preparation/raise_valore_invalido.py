ebfa8f23a2671d25d8abd63286beb8de3eb785d8
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-09-27 08:43:49 +0200

    Update raise_valore_invalido.py

--- pcep_preparation/raise_valore_invalido.py
+++ pcep_preparation/raise_valore_invalido.py
@@ -6,3 +6,10 @@ def calcola_media(valori):
     raise ValoreInvalidoError("Lista di valori vuota")
     media = sum(valori) / len(valori)
     return media
+
+try:
+  valori = []
+  media = calcola_media(valori)
+  print("La media è:", media)
+  except ValoreInvalidoError as e:
+  print("Errore:", e)
