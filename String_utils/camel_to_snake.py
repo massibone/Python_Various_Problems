61386b9a39bab51b192bd44343d2027eba4125a3
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-10-20 08:19:26 +0200

    Update camel_to_snake.py

--- String_utils/camel_to_snake.py
+++ String_utils/camel_to_snake.py
@@ -1,1 +1,9 @@
+import re
 
+def camel_to_snake(name):
+  # Utilizza una regex per inserire un underscore tra le lettere maiuscole e minuscole
+  snake_case = re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
+  return snake_case
+
+# Test della funzione
+print(camel_to_snake("MySampleText")) # Output: "my_sample_text"
