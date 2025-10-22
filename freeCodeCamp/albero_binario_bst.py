7aeb1c447b520287c2828c72fcfb2df7f6e3cf9d
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-10-22 10:04:39 +0200

    Update albero_binario_bst.py

--- freeCodeCamp/albero_binario_bst.py
+++ freeCodeCamp/albero_binario_bst.py
@@ -74,3 +74,16 @@ class BinarySearchTree:
         result = []
         self._inorder_traversal(self.root, result)
         return result
+
+bst = BinarySearchTree()
+nodes = [50, 30, 20, 40, 70, 60, 80]
+delete(bst,40)
+for node in nodes:
+    bst.insert(node)
+
+print('Search for 80:', bst.search(80))
+
+print("Inorder traversal:", bst.inorder_traversal()) # Search for 80: 80
+                                                     # Inorder traversal: [20, 30, 40, 50, 60, 70, 80]
+
+
