264646c9153bb748f87a4470dd13a0c4973f8648
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-11-08 09:10:09 +0100

    Update count_text.py

--- Count_words/count_text.py
+++ Count_words/count_text.py
@@ -8,10 +8,12 @@ for line in handle:
         counts[word] = counts.get(word, 0) + 1
 bigcount = None
 bigword = None
+
 for word, count in list(counts.items()):
     if bigcount is None or count > bigcount:
         bigword = word
         bigcount = count
 
         print(bigword, bigcount)
+
 
