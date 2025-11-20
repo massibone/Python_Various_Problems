5cd83512171eb50b05fc8441d8f5545716fc65c3
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-11-20 08:40:47 +0100

    Update gen_pasword.py

--- freeCodeCamp/gen_pasword.py
+++ freeCodeCamp/gen_pasword.py
@@ -1,3 +1,47 @@
 import re
 import secrets
 import string
+
+
+def generate_password(length, nums, special_chars, uppercase, lowercase):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+       
+        constraints = [
+            (nums, r'\d'),
+            (lowercase, r'[a-z]'),
+            (uppercase, r'[A-Z]'),            
+            (special_chars, fr'[{symbols}]')            
+        ]
+       
+        # Check constraints        
+        if all(
+            constraint <= len(re.findall(pattern, password))
+            for constraint, pattern in constraints
+        ):
+            break
+
+    return password
+   
+new_password = generate_password(length=8, nums=1, special_chars=1, uppercase=1, lowercase=1)
+
+print(new_password)
+#console output--> 9s=ARrlW
+
+'''
+In this way, your code won't run when imported as a module. Otherwise, it will call generate_password() and print the generated password. 
+if __name__ == '__main__':
+    new_password = generate_password()
+    print('Generated password:', new_password)
+'''
