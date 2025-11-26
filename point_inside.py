be9880d3bd4033d01fe1b842e4118bf7094ea8fa
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-11-26 08:31:22 +0100

    Update point_inside.py

--- point_inside.py
+++ point_inside.py
@@ -1,1 +1,19 @@
+import random, math
 
+
+random.seed(1)
+        
+def in_circle(x, origin = [0]*2):
+    """
+        This function determines if a two-dimensional point
+        falls within the unit circle.
+    """
+    if len(x) != 2:
+        return "x is not two-dimensional!"
+    elif distance(x, origin) < 1:
+        return True
+    else:
+        return False
+
+
+print(in_circle((1,1)))
