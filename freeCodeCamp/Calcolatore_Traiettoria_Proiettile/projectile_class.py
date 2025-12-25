3fa0200ac0827f381a1e186232e07241dfc13798
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-12-25 08:43:06 +0100

    Update projectile_class.py

--- freeCodeCamp/Calcolatore_Traiettoria_Proiettile/projectile_class.py
+++ freeCodeCamp/Calcolatore_Traiettoria_Proiettile/projectile_class.py
@@ -1,1 +1,10 @@
 
+if __name__ == "__main__":
+    
+    ball = Projectile(20, 5, 60) # Esempio: 20 m/s, Altezza 5 m, Angolo 60°
+    print(ball)
+    
+    coordinates = ball.calculate_all_coordinates()
+    graph = Graph(coordinates)
+    print(graph.create_trajectory())
+  
