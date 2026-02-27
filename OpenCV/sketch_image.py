e7fa5b1f0ba4309efb55e2ee36019ab2861f59c1
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2026-02-27 08:57:03 +0100

    Update sketch_image.py

--- OpenCV/sketch_image.py
+++ OpenCV/sketch_image.py
@@ -1,13 +1,1 @@
-import cv2
-image=cv2.imread(r'foto_mia.png')
-
-grey_img=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-invert=cv2.bitwise_not(grey_img)
-blur=cv2.GaussianBlur(invert, (31,31),0)
-invertedblur=cv2.bitwise_not(blur)
-sketch=cv2.divide(grey_img,invertedblur,scale=128.0)
-
-cv2.imwrite('sketch.png', sketch)
-
-
 
