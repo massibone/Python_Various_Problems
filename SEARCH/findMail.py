f0dd4425aa06272c57d00a86e7787e498d4eb1f5
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2026-03-07 08:20:21 +0100

    Update findMail.py

--- SEARCH/findMail.py
+++ SEARCH/findMail.py
@@ -1,7 +1,8 @@
 import re
 
 # The RegEx library
-# our regular expression (to find e-mails)
+# our regular expression 
+(to find e-mails)
 # and text to search
 
 regex = '\s[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}[\s]'
